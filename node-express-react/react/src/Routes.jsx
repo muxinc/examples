a30310d20266f67209c8800d21d4cd44874b3319
Jacqui Manzi <jacquimanzi@gmail.com>
2019-03-06 10:01:52 -0800

    Consolidating

--- node-express-react/react/src/Routes.jsx
+++ node-express-react/react/src/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, About, Login } from './containers';
+import { Home, About, Login } from './containers/index';
 
 const Routes = () => (
   <Switch>
