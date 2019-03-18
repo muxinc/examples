4378badae350a98b916c1ccffa3b10fea3f9dffb
Matthew McClure <m@mmcc.io>
2019-03-18 13:44:02 -0700

    we never actually used the server config stuff

--- now-airtable/next.config.js
+++ now-airtable/next.config.js
@@ -1,10 +1,6 @@
 // next.config.js
 module.exports = {
   target: 'serverless',
-  serverRuntimeConfig: {
-    airtableKey: process.env.AIRTABLE_API_KEY,
-    airtableBase: process.env.AIRTABLE_BASE,
-  },
   env: {
     baseUrl: process.env.BASE_URL,
     dev: process.env.NODE_ENV !== 'production',
