cf828bc222d7c0827d3f252708fad32680a6d12d
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-08-26 14:13:16 -0700

    make the about page a modal overlay

--- stream.new/components/layout.js
+++ stream.new/components/layout.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useDropzone } from 'react-dropzone';
 import { breakpoints, transitionDuration } from '../style-vars';
 import Globe from './globe';
+import InfoModal from './info-modal';
 // import { MUX_HOME_PAGE_URL } from '../constants';
 
-const InfoLink = () => <Link href="/about"><a>Info</a></Link>;
 const GlobeLink = () => <Link href="/"><a><Globe /></a></Link>;
+// const HomeLink = () => <Link href="/"><a><Globe /></a></Link>;
 
 export default function Layout ({
   title,
@@ -14,15 +16,17 @@ export default function Layout ({
   metaTitle,
   metaDescription,
   image,
-  footerLinks = [<InfoLink />, <GlobeLink />],
   onFileDrop,
   darkMode,
   children,
 }) {
+  const [isModalOpen, setModalOpen] = useState(false);
   const { getRootProps, isDragActive } = useDropzone({ onDrop: onFileDrop });
+  const isDroppablePage = !!onFileDrop;
+  const containerProps = isDroppablePage ? getRootProps() : {};
 
   return (
-    <div className="container" {...getRootProps()}>
+    <>
       <Head>
         <title>stream.new</title>
         <link rel="icon" href="/favicon.ico" />
@@ -40,133 +44,153 @@ export default function Layout ({
         )}
         {image && <meta property="twitter:image" content={image} />}
       </Head>
-      <div className={`drag-overlay ${isDragActive ? 'active' : ''}`}><h1>Upload to stream.new</h1></div>
+      <div className="container" {...containerProps}>
+        <div className={`drag-overlay ${isDragActive ? 'active' : ''}`}><h1>Upload to stream.new</h1></div>
 
-      <main>
-        {children}
-      </main>
-      <footer>
-        {footerLinks.map((link, idx) => <div key={idx} className="footer-link">{link}</div>)} {/* eslint-disable-line react/no-array-index-key */}
-      </footer>
+        <main>
+          <div className="modal-wrapper"><InfoModal close={() => setModalOpen(false)} /></div>
+          {children}
+        </main>
+        <footer>
+          <div className="footer-link"><a role="presentation" onClick={() => setModalOpen(true)}>Info</a></div>
+          <div className="footer-link"><GlobeLink /></div>
+        </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          min-height: -webkit-fill-available;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background: ${darkMode ? '#111' : '#f8f8f8'};
-          transition: background ${transitionDuration} ease;
-        }
-        .drag-overlay {
-          height: 100%;
-          width: 100%;
-          position: absolute;
-          z-index: 1;
-          background-color:  rgba(226, 253, 255, 0.95);
-          transition: 0.5s;
-          display: none;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+        <style jsx>{`
+          .modal-wrapper {
+            display: ${isModalOpen ? 'flex' : 'none'};
+            position: absolute;
+            top: 0;
+            height: 100%;
+            z-index: 2;
+            width: 100%;
+          }
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background: ${darkMode ? '#111' : '#f8f8f8'};
+            transition: background ${transitionDuration} ease;
+          }
+          .drag-overlay {
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            z-index: 1;
+            background-color:  rgba(226, 253, 255, 0.95);
+            transition: 0.5s;
+            display: none;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
 
-        .drag-overlay h1 {
-          font-size: 46px;
-          line-height: 46px;
-          text-align: center;
-        }
-
-        .drag-overlay.active {
-          display: flex;
-        }
-
-        main {
-          padding: 20px;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          max-width: 1100px;
-        }
-
-        footer {
-          width: 100%;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          padding-left: 30px;
-          padding-right: 30px;
-          padding-bottom: 30px;
-          height: 120px;
-        }
-
-        .footer-link {
-          font-size: 26px;
-          line-height: 33px;
-        }
-
-        .footer-link :global(a), .footer-link :global(a:visited) {
-          color: ${darkMode ? '#ccc' : '#383838'};
-          text-decoration: none;
-        }
-
-        @media only screen and (min-width: ${breakpoints.md}px) {
           .drag-overlay h1 {
-            font-size: 96px;
-            line-height: 120px;
+            font-size: 46px;
+            line-height: 46px;
+            text-align: center;
           }
-        }
-      `}
-      </style>
 
-      <style jsx global>{`
-        html,
-        body,
-        a,
-        a:visited {
-          padding: 0;
-          margin: 0;
-          font-family: Akkurat;
-          color: #383838;
-        }
+          .drag-overlay.active {
+            display: flex;
+          }
 
-        h1 {
-          font-family: Akkurat;
-          font-style: normal;
-          font-weight: normal;
-          font-size: 36px;
-          line-height: 45px;
-          margin: 0;
-          text-align: left;
-          color: #383838;
-        }
+          main {
+            padding: 20px;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            max-width: 1100px;
+          }
 
-        h2 {
-          font-family: Akkurat;
-          font-style: normal;
-          font-weight: normal;
-          font-size: 26px;
-          line-height: 33px;
-          color: #383838;
-        }
+          footer {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding-left: 30px;
+            padding-right: 30px;
+            padding-bottom: 30px;
+            height: 120px;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
+          .footer-link {
+            font-size: 26px;
+            line-height: 33px;
+          }
 
-        @media only screen and (min-width: ${breakpoints.md}px) {
+          .footer-link :global(a), .footer-link :global(a:visited) {
+            color: ${darkMode ? '#ccc' : '#383838'};
+            text-decoration: none;
+            cursor: pointer;
+          }
+
+          @media only screen and (min-width: ${breakpoints.md}px) {
+            .drag-overlay h1 {
+              font-size: 96px;
+              line-height: 120px;
+            }
+          }
+        `}
+        </style>
+
+        <style jsx global>{`
+          html, body, #__next, .container {
+            height: 100%;
+          }
+
+          html,
+          body,
+          a,
+          a:visited {
+            padding: 0;
+            margin: 0;
+            font-family: Akkurat;
+          }
+
+          a, a:visited {
+            cursor: pointer;
+            color: ${darkMode ? '#ccc' : '#383838'};
+          }
+
+          h1, h2 {
+            color: ${darkMode ? '#ccc' : '#383838'};
+          }
+
           h1 {
-            font-size: 64px;
-            line-height: 80px;
+            font-family: Akkurat;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 36px;
+            line-height: 45px;
+            margin: 0;
             text-align: left;
-            color: #383838;
           }
-        }
-      `}
-      </style>
-    </div>
+
+          h2 {
+            font-family: Akkurat;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 26px;
+            line-height: 33px;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+
+          @media only screen and (min-width: ${breakpoints.md}px) {
+            h1 {
+              font-size: 64px;
+              line-height: 80px;
+              text-align: left;
+            }
+          }
+        `}
+        </style>
+      </div>
+    </>
   );
 }
