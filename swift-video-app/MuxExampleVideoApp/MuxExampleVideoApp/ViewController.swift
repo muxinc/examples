45430f19c2811d0be7af6f7ef00e3f850ddfe2e4
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-04-07 11:19:00 -0700

    launch the app into avplayerviewcontroller

--- swift-video-app/MuxExampleVideoApp/MuxExampleVideoApp/ViewController.swift
+++ swift-video-app/MuxExampleVideoApp/MuxExampleVideoApp/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: AVPlayerViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let url = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
+        player = AVPlayer(url: url!)
+        player!.play()
         // Do any additional setup after loading the view.
     }
 
