8742dd9c372e17996d7ce3a20c97b867ff79f25e
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-05-12 09:15:08 -0700

    swift-video-app: Update HLS url to stream.mux.com not stream.new
    :facepalm:

--- swift-video-app/MuxExampleVideoApp/MuxExampleVideoApp/ViewController.swift
+++ swift-video-app/MuxExampleVideoApp/MuxExampleVideoApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: AVPlayerViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://stream.new/v/qxb01i6T202018GFS02vp9RIe01icTcDCjVzQpmaB00CUisJ4.m3u8")
+        let url = URL(string: "https://stream.mux.com/qxb01i6T202018GFS02vp9RIe01icTcDCjVzQpmaB00CUisJ4.m3u8")
         player = AVPlayer(url: url!)
         let playName = "iOS AVPlayer"
         let playerData = MUXSDKCustomerPlayerData(environmentKey: "ENV_KEY")
