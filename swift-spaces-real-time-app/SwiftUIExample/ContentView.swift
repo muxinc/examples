684e0c16ee36742751175c6cc7010e2f4dc0cf6a
AJ Lauer Barinov <abarinov@mux.com>
2023-03-29 13:18:28 -0700

    Add SwiftUI example app target

---
+++ swift-spaces-real-time-app/SwiftUIExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  Created for SwiftUIExample.
+//  
+//  Copyright © 2023 Mux, Inc.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-spaces-real-time-app/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Created for SwiftUIExample.
+//  
+//  Copyright © 2023 Mux, Inc.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
