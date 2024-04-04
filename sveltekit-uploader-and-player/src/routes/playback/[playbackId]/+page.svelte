086d37a82684df4338aaad44baf2938e4f683d83
Darius Cepulis <dcepulis@mux.com>
2024-04-04 15:51:31 -0500

    SEO exmaple

--- sveltekit-uploader-and-player/src/routes/playback/[playbackId]/+page.svelte
+++ sveltekit-uploader-and-player/src/routes/playback/[playbackId]/+page.svelte
@@ -3,6 +3,9 @@
 	import '@mux/mux-player';
 
 	let playbackId = $derived($page.params.playbackId);
+	const title = 'View this video created with Mux + SvelteKit';
+	const description =
+		'This video was uploaded and processed by Mux in an example SvelteKit application.';
 </script>
 
 <div class="px-8 py-4 mb-8 text-center bg-green-500/30 rounded-full">
@@ -21,3 +24,31 @@
 		back home
 	</a> to upload another video.
 </p>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta property="og:type" content="video" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta
+		property="og:image"
+		content="https://image.mux.com/{playbackId}/thumbnail.png?width=1200&height=630&fit_mode=pad"
+	/>
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta
+		property="twitter:image"
+		content="https://image.mux.com/{playbackId}/thumbnail.png?width=1200&height=600&fit_mode=pad"
+	/>
+	<meta property="twitter:image:width" content="1200" />
+	<meta property="twitter:image:height" content="600" />
+	<!-- 
+		These tags should be sufficient for social sharing.
+  	However, if you're really committed video SEO, I'd suggest adding ld+json, as well.
+  	https://developers.google.com/search/docs/appearance/structured-data/video
+	-->
+</svelte:head>
